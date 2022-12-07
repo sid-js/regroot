0f989f1db8d72cdec45d9f5c7e5fcd408846a77c
sid-js <sidhsreejil@gmail.com>
2022-12-07 17:33:57 +0530

    employer_logo fix

--- components/JobCard.jsx
+++ components/JobCard.jsx
@@ -12,7 +12,7 @@ const JobCard = ({ job }) => {
       <div class="absolute -inset-0.5 hidden group-hover:block bg-gradient-to-r from-brand to-green-400 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
       <div className="relative flex flex-col items-center w-full px-5 py-5 text-white transition duration-200 rounded-lg bg-secondarybg hover:scale-105 hover:translate-y-1 hover:cursor-pointer hover:drop-shadow-md">
         <div className="flex flex-row items-center justify-start w-full gap-4">
-          {job.employer_logo?(
+          {job?.employer_logo?(
           <div className="flex items-center justify-center w-16 h-16 bg-white">
             <img
             className="object-cover w-16 p-1 overflow-hidden "
