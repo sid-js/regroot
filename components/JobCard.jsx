c86b719e7fac8a5c3cd92efb62e738cc8b027101
sid-js <sidhsreejil@gmail.com>
2022-12-08 19:30:57 +0530

    Fixed favicon and Card Glow

--- components/JobCard.jsx
+++ components/JobCard.jsx
@@ -9,8 +9,8 @@ import TimeAgo from "react-timeago";
 const JobCard = ({ job }) => {
   return (
     <div class="relative group md:w-[75%] w-[95%]">
-      <div class="absolute -inset-0.5 hidden group-hover:block bg-gradient-to-r from-brand to-green-400 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-      <div className="relative flex flex-col items-center w-full px-5 py-5 text-white transition duration-200 rounded-lg bg-secondarybg hover:scale-105 hover:translate-y-1 hover:cursor-pointer hover:drop-shadow-md">
+      <div class="absolute -inset-0.5 hidden group-hover:block bg-gradient-to-r from-brand to-green-400 rounded-lg blur-md opacity-90 scale-105 -translate-y-1 group-hover:opacity-100 transition-all duration-400 group-hover:duration-300 animate-tilt"></div>
+      <div className="relative flex flex-col items-center w-full px-5 py-5 text-white transition duration-200 rounded-lg bg-secondarybg hover:scale-105 hover:cursor-pointer hover:drop-shadow-md">
         <div className="flex flex-row items-center justify-start w-full gap-4 md:gap-4">
           {job?.employer_logo?(
           <div className="flex items-center justify-center w-16 h-16 bg-white shrink-0">
